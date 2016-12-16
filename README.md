caf3f46dad2e5707f55b4ea801f772b779bbc1a6
Jose Miguel Gallas Olmedo <jgallaso@redhat.com>
2016-12-16 15:59:18 +0100

    Adds aslant and semistandar

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # node-backend-sample
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square)](https://github.com/Flet/semistandard)ﬁ
 A nodeJS backend made with Express for learning purposes
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 // Routes
-const index = require("./routes/index.js");
+const index = require('./routes/index.js');
 
-app.use("/", index);
+app.use('/', index);
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
