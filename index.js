40cfc4912691ae4fb932b388b56499b57d9462a6
Jose Miguel Gallas Olmedo <jgallaso@redhat.com>
2016-12-16 14:07:44 +0100

    Add routes with Router

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Routes
+const index = require("./routes/index.js");
+
+app.use("/", index);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Example app listening on port 3000!')
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.get('/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}!`)
+})
+
+module.exports = router;
